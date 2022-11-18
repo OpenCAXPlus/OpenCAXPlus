411642d0e1886934fb3c9e89976345220d095bf7
chengxiaoxing <0001001bill@gmail.com>
2022-11-18 15:46:44 -0500

    add custom target type to framework

---
+++ cmake/OCPCustomUserProgram.cmake
@@ -0,0 +1,50 @@
+macro(OCP_Custom_User_Program)
+  set(options USE_CXX)
+  set(oneValueArgs LOG SOLVER NAME ROOT)
+  cmake_parse_arguments(OCPUser "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  message(STATUS " Adding your program ...")
+  message(STATUS " ROOT: ${OCPUser_ROOT}")
+  message(STATUS " PROGRAM_NAME: ${OCPUser_NAME}")
+  message(STATUS " LOG: ${OCPUser_LOG}")
+  message(STATUS " SOLVER: ${OCPUser_SOLVER}")
+
+  option(ENABLE_TEST "Enable testing" ON)
+  if(ENABLE_TEST)
+    include(CTest)
+  endif()
+
+  include(OCPPrepareVariables)
+
+  set(OCP_LIFECYCLE
+      Custom_Console
+      CACHE INTERNAL "")
+
+  set(OCP_BACKEND_LOG
+      ${OCPUser_LOG}
+      CACHE INTERNAL "")
+
+  set(OCP_BACKEND_SOLVER
+      ${OCPUser_SOLVER}
+      CACHE INTERNAL "")
+
+  set(USER_PROGRAM
+      ${OCPUser_NAME}
+      CACHE INTERNAL "")
+
+  set(OCP_ROOT
+      ${OCPUser_ROOT}
+      CACHE INTERNAL "")
+
+  add_subdirectory(${OCP_ROOT}/toolkit ${PROJECT_BINARY_DIR}/toolkit)
+
+  target_include_directories(
+    ${OCPUser_TARGET} PUBLIC ${OCP_ROOT}/toolkit
+                             ${OCP_ROOT}/framework/lifecycle/${OCP_LIFECYCLE})
+  target_link_libraries(${OCPUser_TARGET} PUBLIC OCP::Toolkit::Log
+                                                 OCP::Toolkit::Solver)
+  add_subdirectory(${OCP_ROOT}/framework/lifecycle
+                   ${PROJECT_BINARY_DIR}/framework/lifecycle)
+
+endmacro(OCP_Custom_User_Program)
---
+++ framework/lifecycle/Custom_Console/main.c
@@ -0,0 +1,14 @@
+
+#include "global.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int    argc;
+char** argv;
+
+int main(int _argc, const char* _argv[])
+{
+    INFO("The custom user program");
+    return 0;
+}
