3a5f05d39623e713276ac326de4580b5981e2159
chengxiaoxing <0001001bill@gmail.com>
2022-12-17 01:40:45 -0500

    switch to 7z for compression, because vtk is fairly large
    also add custom command of compress_{name} to conveniently compress the external lib folder

--- cmake/OCPToolkitExternal.cmake
+++ cmake/OCPToolkitExternal.cmake
@@ -11,11 +11,26 @@ macro(OCP_Toolkit_External)
       "Add external library ${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}, link to ${OCPExternal_LIBRARIES}, includes ${OCPExternal_INCLUDE_DIRECTORIES}"
   )
 
+  add_custom_target(
+    compress_${external_name}
+    DEPENDS
+      ${CMAKE_CURRENT_SOURCE_DIR}/${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}.7z
+    COMMENT "Compress the ${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}")
+
+  add_custom_command(
+    OUTPUT
+      ${CMAKE_CURRENT_SOURCE_DIR}/${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}.7z
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMAND "${CMAKE_COMMAND}" "-E" "tar" "cf"
+            "${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}.7z" "--format=7zip" "--" "${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}/*"
+    COMMENT "7zip compress ${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}")
+
+
   if(NOT EXISTS
      ${CMAKE_CURRENT_SOURCE_DIR}/${OCPExternal_SOFTWARE}-${OCPExternal_VERSION})
     execute_process(
-      COMMAND "${CMAKE_COMMAND}" "-E" "tar" "xz"
-              "${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}.tar.gz"
+      COMMAND "${CMAKE_COMMAND}" "-E" "tar" "xf" 
+              "${OCPExternal_SOFTWARE}-${OCPExternal_VERSION}.7z" "--format=7zip"
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
 
