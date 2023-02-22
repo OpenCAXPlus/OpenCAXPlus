3017d6560922d6ce44c390ae7508f0af5d4de52b
chengxiaoxing <0001001bill@gmail.com>
2023-02-22 17:07:07 -0500

    update link script

--- starter/fengsim/LD.sh
+++ starter/fengsim/LD.sh
@@ -1,3 +1,5 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/xcheng/codes/OpenCAXPlus/OpenCAXPlusSDK/toolkit/VTK/external/install/VTK-8.1.0/lib:/home/xcheng/codes/OpenCAXPlus/OpenCAXPlusSDK/toolkit/gmsh/external/install/gmsh-3.0.6/lib:/home/xcheng/codes/OpenCAXPlus/OpenCAXPlusSDK/toolkit/OCE/external/install/OCE-0.18.3/lib
+cache=$HOME/opencaxplus/cache
 
-export PATH=$PATH:/home/xcheng/codes/OpenCAXPlus/OpenCAXPlusSDK/toolkit/Qt/external/install/5.12.10/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cache/VTK/8.1.0/install/lib:$cache/gmsh/3.0.6/install/lib:$cache/OCE/0.18.3/install/lib
+
+export PATH=$PATH:$cache/Qt/5.12.10/install/bin
