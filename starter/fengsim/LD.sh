7a174ad9d898b4cc6e57c3f7a11812a65e14edb5
chengxiaoxing <0001001bill@gmail.com>
2023-04-15 15:59:49 -0400

    update ld

--- starter/fengsim/LD.sh
+++ starter/fengsim/LD.sh
@@ -1,5 +1,5 @@
 cache=$HOME/opencaxplus/cache
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cache/VTK/8.1.0/install/lib:$cache/gmsh/3.0.6/install/lib:$cache/OCE/0.18.3/install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cache/VTK/8.1.0/install/lib:$cache/gmsh/3.0.6/install/lib:$cache/OCE/0.18.3/install/lib:$cache/Qt/5.12.10/install/lib
 
 export PATH=$PATH:$cache/Qt/5.12.10/install/bin
