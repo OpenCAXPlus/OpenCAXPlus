5c2d25e977efee9f45a0b8da4658145743f7c62b
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-13 20:33:23 +0800

    add the update subcommand

---
+++ cli/cmd/update.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2023 Xiaoxing Cheng <hello@chengxiaoxing.me>
+*/
+package cmd
+
+import (
+	"OpenCAXPlusCli/pkg"
+	"runtime"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// the update command will
+// 1. download the install.sh and packages.yml file
+// 2. source the install.sh file
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "ocp update",
+	Long: `
+	The command will update the packages.yml file and the ocp cli.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		log.Debug("update command")
+		log.Debug("System is ", runtime.GOOS)
+
+		pkg.Update()
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
---
+++ cli/pkg/update.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// ! TODO need to run the Download recursively on config dependencies
+func Update() {
+
+	// Read the packages.yml file
+	homeDir, _ := os.UserHomeDir()
+	downloadPath := filepath.Join(homeDir, "ocp")
+	downloadUrl := "https://ocp-download.oss-cn-hongkong.aliyuncs.com/packages.yml"
+	downloadFile := filepath.Join(downloadPath, "packages.yml")
+	err := DownloadFile(downloadUrl, downloadFile)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Successfully downloaded ", downloadUrl, " to ", downloadFile)
+
+	downloadPath = filepath.Join(homeDir, "ocp", "download")
+	downloadUrl = "https://ocp-download.oss-cn-hongkong.aliyuncs.com/install.sh"
+	downloadFile = filepath.Join(downloadPath, "install.sh")
+	err = DownloadFile(downloadUrl, downloadFile)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Successfully downloaded ", downloadUrl, " to ", downloadFile)
+
+	executeCommand("source " + downloadFile)
+}
