2a92a98eeb73960ed2fa053e49d9159c393ce137
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-18 20:55:52 +0800

    update the framework cmake and lifecycle

--- framework/lifecycle/base_console/main.c
+++ framework/lifecycle/base_console/main.c
@@ -1,5 +1,5 @@
 
-#include "console.h"
+#include "define.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- framework/lifecycle/basic/dummy.c
+++
@@ -1,6 +0,0 @@
-#include "console.h"
-#include <stdio.h>
-ocpInfoFunc  ocp_info  = printf;
-ocpDebugFunc ocp_debug = printf;
-
-int app(int _argc, char* _argv[]) { ocp_info("Reading input"); }
